0ba6f337746e09fbe9fb6e44feb89a70eeae0f67
chtak <chtakyol@gmail.com>
2021-07-21 12:32:14 +0300

    model builded

--- model/lenet.py
+++ model/lenet.py
@@ -1,42 +1,44 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
+import torchvision
+import torchvision.transforms as transforms
 
 class LeNet(nn.Module):
     def __init__(self):
         super(LeNet, self).__init__()
-        self.c1 = nn.Conv2d(
-            in_channels=1, out_channels=6, 
-            kernel_size=5, stride=1, padding=2
-            )
+        self.c1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, padding=2)
         self.s2 = nn.AvgPool2d(kernel_size=2, stride=2)
-        self.c3 = nn.Conv2d(
-            in_channels=6, out_channels=16,
-            kernel_size=5
-        )
+        self.c3 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
         self.s4 = nn.AvgPool2d(kernel_size=2, stride=2)
-        self.c5 = nn.Conv2d(
-            in_channels=16, out_channels=120,
-            kernel_size=5
-        )
+        self.l5 = nn.Linear(in_features=400, out_features=120)
         self.l6 = nn.Linear(in_features=120, out_features=84)
         self.l7 = nn.Linear(in_features=84, out_features=10)
 
-        self.tanh = nn.Tanh()
+        self.flatten = nn.Flatten()
 
     def forward(self, x):
+
         x = self.c1(x)
-        x = self.tanh(x)
+        x = torch.sigmoid(x)
         x = self.s2(x)
+
         x = self.c3(x)
-        x = self.tanh(x)
+        x = torch.sigmoid(x)
         x = self.s4(x)
-        x = self.c5(x)
-        x = self.tanh(x)
-        x = torch.flatten(x)
+
+        x = self.flatten(x)
+        x = self.l5(x)
+        x = torch.sigmoid(x)
+
         x = self.l6(x)
-        x = self.tanh(x)
+        x = torch.sigmoid(x)
+        
         x = self.l7(x)
+        x = torch.sigmoid(x)
+        
         return x
 
 if __name__ == '__main__':
@@ -44,4 +46,4 @@ if __name__ == '__main__':
     net = LeNet()
     input = torch.randn(1, 1, 28, 28)
     out = net.forward(input)
-    print(out)
+    print(out.shape)
--- train.py
+++ train.py
@@ -2,25 +2,64 @@ from torchvision import datasets
 from torch.utils.data import DataLoader
 import torch.optim as optim
 import torch.nn as nn
+import torch
 from model import lenet
 
-def train():
-    train_dataset = datasets.MNIST(
-        root='datasets/mnist', train=True, download=False)
-    test_dataset = datasets.MNIST(
-        root='datasets/mnist', train=False, download=False)
-    
-    train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
-    test_loader = DataLoader(dataset=test_dataset, batch_size=64, shuffle=True)
-    dataset_sizes = {
-        'train':len(train_dataset), 'test':len(test_dataset)
-        }
-    print(dataset_sizes)
+def train(model, criterion, optimizer, train_loader, validation_loader, epochs):
+    i=100
+    stuff = {'training_loss': [], 'validation_loss': []}
+
+    for epoch in range(epochs):
+        for i, (x,y) in enumerate(train_loader):
+            optimizer.zero_grad()
+            z = model(x.view(-1, 28*28))
+            loss = criterion(z, y)
+            loss.backward()
+            optimizer.step()
+            stuff['training_loss'].append(loss.data.item())
+        
+        correct = 0
+        for (x,y) in validation_loader:
+            z = model(x.view(-1, 28*28))
+            _, label = torch.max(z)
+            correct += (label == y).sum().item()
+        
+        accuracy = 100 * (correct / len(validation_dataset))
+        stuff['validation_accuracy'].append(accuracy)
+    return stuff
 
 
 if __name__ == '__main__':
-    train()
-    l_rate = 0.01
     model = lenet.LeNet()
+    
     criterion = nn.CrossEntropyLoss()
+    
+    l_rate = 0.01
     optimizer = optim.Adam(model.parameters(), lr=l_rate)
+
+    train_dataset = datasets.MNIST(
+        root='datasets/mnist', train=True, download=False)
+    validation_dataset = datasets.MNIST(
+        root='datasets/mnist', train=False, download=False)
+    
+    train_loader = DataLoader(dataset=train_dataset, batch_size=64, shuffle=True)
+    
+    validation_loader = DataLoader(dataset=validation_dataset, batch_size=64, shuffle=True)
+    
+    dataset_sizes = {
+        'train':len(train_dataset), 'val':len(validation_dataset)
+        }
+
+    print(dataset_sizes)
+    for batch_ndx, sample in enumerate(train_loader):
+        print(sample.inp.is_pinned())
+        print(sample.tgt.is_pinned())
+
+    # train(
+    #     model=model,
+    #     criterion=criterion,
+    #     optimizer=optimizer,
+    #     train_loader=train_loader,
+    #     validation_loader=validation_loader,
+    #     epochs=10
+    # )
